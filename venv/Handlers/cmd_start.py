ceb2d7c94012de2e7afa2f22a2d3a6f45eb7ad25
Sfinks7802 <sfinks7802@gmail.com>
2024-06-13 15:36:51 +0300

    added checking for sub

--- venv/Handlers/cmd_start.py
+++ venv/Handlers/cmd_start.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ChatMemberLeft
+from PrivatInfo import bot
 
 
 router = Router()
@@ -8,6 +9,6 @@ router = Router()
 
 @router.message(Command('start'))
 async def cmd_start(message: Message):
-    await message.answer(
-        'hello'
-    )
+    user_channel_status = await bot.get_chat_member(chat_id='-1001841308905', user_id=message.from_user.id)
+    if isinstance(user_channel_status, ChatMemberLeft):
+        await message.answer('text if not in group')
